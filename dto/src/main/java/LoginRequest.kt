4fefcb2fa1cda9dc70dd70075d976a57d299856f
Raman Makarevich <raman.makarewicz@gmail.com>
2023-04-06 00:41:57 +0200

    Separate 'Login' and 'Register' requests

--- dto/src/main/java/LoginRequest.kt
+++ dto/src/main/java/LoginRequest.kt
@@ -3,7 +3,7 @@ package com.apsl.glideapp.common.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AuthRequest(
+data class LoginRequest(
     val username: String,
     val password: String
 )
