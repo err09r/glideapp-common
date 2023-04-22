4699ebd45d631fa8a55e42d1080410146fc0a784
Raman Makarevich <raman.makarewicz@gmail.com>
2023-04-22 20:04:10 +0200

    Update RideStateDto

--- dto/build.gradle.kts
+++ dto/build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
 dependencies {
     api(project(":models"))
     implementation(libs.kotlin.serialization.json)
+    implementation(libs.kotlin.datetime)
 }
--- dto/src/main/kotlin/RideStateDto.kt
+++ dto/src/main/kotlin/RideStateDto.kt
@@ -1,11 +1,12 @@
 package com.apsl.glideapp.common.dto
 
+import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed interface RideStateDto {
     @Serializable
-    data class Started(val rideId: String) : RideStateDto
+    data class Started(val rideId: String, val dateTime: LocalDateTime) : RideStateDto
 
     @Serializable
     object Paused : RideStateDto
