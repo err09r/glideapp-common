6914f35c98816e8c2f34fe58aebe52fd729f6c26
Raman Makarevich <raman.makarewicz@gmail.com>
2023-05-17 00:21:26 +0200

    Add RideDto

---
+++ dto/src/main/kotlin/RideDto.kt
@@ -0,0 +1,16 @@
+package com.apsl.glideapp.common.dto
+
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RideDto(
+    val id: String,
+    val userId: String,
+    val startAddress: String?,
+    val finishAddress: String?,
+    val startDateTime: LocalDateTime,
+    val finishDateTime: LocalDateTime,
+    val distance: Double,
+    val averageSpeed: Double
+)
