1d512534c7b60378ec101f077c117fe32658013e
Raman Makarevich <raman.makarewicz@gmail.com>
2023-05-25 19:24:55 +0200

    Update GlideConfigurationDto

--- dto/src/main/kotlin/GlideConfigurationDto.kt
+++ dto/src/main/kotlin/GlideConfigurationDto.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GlideConfigurationDto(
     val countryCode: String,
+    val unlockingFee: Double,
     val farePerMinute: Double
 )
