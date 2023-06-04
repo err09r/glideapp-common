7fe6b1f5110dc5f9cebfc5529954fab324d65ba2
Raman Makarevich <raman.makarewicz@gmail.com>
2023-06-04 21:46:39 +0200

    Add TransactionRequest

---
+++ dto/src/main/kotlin/TransactionRequest.kt
@@ -0,0 +1,10 @@
+package com.apsl.glideapp.common.dto
+
+import com.apsl.glideapp.common.models.TransactionType
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TransactionRequest(
+    val type: TransactionType,
+    val amount: Double
+)
