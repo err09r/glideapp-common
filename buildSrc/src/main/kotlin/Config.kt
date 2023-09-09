0b3234d95bc249eb6a6a6dc96ad454018e5f9504
Raman Makarevich <raman.makarewicz@gmail.com>
2023-09-09 14:01:08 +0200

    Update AppConfigDto

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,7 +4,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val group = "com.apsl.glideapp.common"
-    const val version = "0.0.45"
+    const val version = "0.0.46"
 
     val javaVersion = JavaVersion.VERSION_1_8
     const val jvmTarget = "1.8"
--- dto/src/main/kotlin/AppConfigDto.kt
+++ dto/src/main/kotlin/AppConfigDto.kt
@@ -3,8 +3,4 @@ package com.apsl.glideapp.common.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AppConfigDto(
-    val unlockDistance: Double,
-    val ridingZones: List<ZoneDto>,
-    val noParkingZones: List<ZoneDto>
-)
+data class AppConfigDto(val unlockDistance: Double)
