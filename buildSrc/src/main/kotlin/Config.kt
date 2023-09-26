215ee8bea5a94b3c769ee1d191f4bec8e64ee00c
Raman Makarevich <raman.makarewicz@gmail.com>
2023-09-26 22:49:42 +0200

    Add 'calculateDistance' with different parameters

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,7 +4,7 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val group = "com.apsl.glideapp.common"
-    const val version = "0.1.4"
+    const val version = "0.1.5"
 
     val javaVersion = JavaVersion.VERSION_1_8
 
