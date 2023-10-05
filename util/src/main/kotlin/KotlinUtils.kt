d1e9c36db7ed43beb35e97aa2b4ac162fa47aafd
Roman Makarewicz <roma123makar@mail.ru>
2023-10-05 11:40:19 +0200

    Add 'dropEvery' function

--- util/src/main/kotlin/KotlinUtils.kt
+++ util/src/main/kotlin/KotlinUtils.kt
@@ -1,3 +1,5 @@
+@file:Suppress("Unused")
+
 package com.apsl.glideapp.common.util
 
 fun String.capitalized(): String {
@@ -5,3 +7,20 @@ fun String.capitalized(): String {
 }
 
 fun Double.format(digits: Int): String = "%.${digits}f".format(this)
+
+/**
+ * Returns a list containing all elements except every [n]-th element.
+ *
+ * @throws IllegalArgumentException if [n] is negative.
+ */
+fun <T> Iterable<T>.dropEvery(n: Int): List<T> {
+    require(n >= 0) { "Requested element count $n is less than zero." }
+    if (n == 0) return toList()
+    val list = ArrayList<T>()
+    for ((i, item) in this.withIndex()) {
+        if ((i + 1) % n != 0) {
+            list.add(item)
+        }
+    }
+    return list
+}
