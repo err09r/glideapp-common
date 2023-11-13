f8d6f31a5e1297e31dc796d92500e9e19197e030
Roman Makarewicz <roma123makar@mail.ru>
2023-11-13 16:48:20 +0100

    Update utils

--- models/src/main/kotlin/Coordinates.kt
+++ models/src/main/kotlin/Coordinates.kt
@@ -51,4 +51,4 @@ class Coordinates {
     }
 }
 
-fun List<Coordinates>.asPairs(): Array<Pair<Double, Double>> = this.map(Coordinates::asPair).toTypedArray()
+fun List<Coordinates>.asPairs(): List<Pair<Double, Double>> = this.map(Coordinates::asPair)
