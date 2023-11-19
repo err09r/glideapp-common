23609b2ed7928a563fe9001d38e92c6187e4ac7a
Roman Makarewicz <raman.makarewicz@gmail.com>
2023-11-19 15:19:25 +0100

    Add extensions in CollectionUtils.kt

--- util/src/main/kotlin/CollectionUtils.kt
+++ util/src/main/kotlin/CollectionUtils.kt
@@ -52,3 +52,19 @@ fun <T> List<T>.compress(n: Int): List<T> {
         }
     }
 }
+
+fun <T, R : Comparable<R>> Iterable<T>.minOByOrDefault(defaultValue: T, selector: (T) -> R): T {
+    return this.minByOrNull(selector) ?: defaultValue
+}
+
+fun <T, R : Comparable<R>> Iterable<T>.minOfOrDefault(defaultValue: R, selector: (T) -> R): R {
+    return this.minOfOrNull(selector) ?: defaultValue
+}
+
+fun <T, R : Comparable<R>> Iterable<T>.maxByOrDefault(defaultValue: T, selector: (T) -> R): T {
+    return this.maxByOrNull(selector) ?: defaultValue
+}
+
+fun <T, R : Comparable<R>> Iterable<T>.maxOfOrDefault(defaultValue: R, selector: (T) -> R): R {
+    return this.maxOfOrNull(selector) ?: defaultValue
+}
